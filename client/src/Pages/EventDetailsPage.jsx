a88cb2d9030ff086a17e9b7211dc056d27091dc0
Ivan Kozlovsky <ivan.kz5565@gmail.com>
2025-06-07 23:15:18 +0300

    changed the EventDetailsPage, its styles and created api point votes/guests

--- client/src/Pages/EventDetailsPage.jsx
+++ client/src/Pages/EventDetailsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import "@/styles/EventDetailsPage.css";
@@ -7,7 +7,7 @@ import Modal from "@/components/Main/Modal";
 import Header from "@/components/Header.jsx";
 import Footer from "@/components/Footer.jsx";
 
-const EventDetailsPage = () => {
+const EventDetailsPage_old = () => {
   const [vote_count, setVoteCount] = useState(0);
   const [response, setResponse] = useState(null);
   const [showModal, setShowModal] = useState(false);
@@ -16,6 +16,22 @@ const EventDetailsPage = () => {
   const event = location.state;
   const navigate = useNavigate();
 
+
+  useEffect(() => {
+    if (event) {
+      axios.get("http://localhost:8080/api/votes/count", {
+        params:
+        {
+          id: event._id,
+          vote: true
+        }
+      })
+        .then((res) => {
+          setVoteCount(res.data);
+          console.log(res.data);
+        });
+    }
+  }, [event])
 
   if (!event) {
     return (
@@ -36,7 +52,7 @@ const EventDetailsPage = () => {
     setResponse(tempResponce);
     axios.post("http://localhost:8080/api/votes", {
       _vote: tempResponce,
-      _event: id,
+      _event: event._id,
       _user: userId,
     })
   }
@@ -49,58 +65,171 @@ const EventDetailsPage = () => {
     }
   };
 
-  useEffect(() => {
-    axios.get("http://localhost:8080/api/votes/count", {
-      params:
-      {
-        id: event._id,
-        vote: true
-      }
-    })
-      .then((res) => {
-        setVoteCount(res.data);
-        console.log(res.data);
-      });
-
-  }, [])
-
   return (
     <div className="event-details-page">
       <Header />
-      <h1>{event.name}</h1>
-      <p><strong>Опис:</strong> {event.describe}</p>
-      <p><strong>Дата:</strong> {new Date(event.date).toLocaleDateString()}</p>
-      <p><strong>Категорія:</strong> {event.category}</p>
+      <div className="event-container">
+        <h1>{event.name}</h1>
+        <p><strong>Опис:</strong> {event.describe}</p>
+        <p><strong>Дата:</strong> {new Date(event.date).toLocaleDateString()}</p>
+        <p><strong>Категорія:</strong> {event.category}</p>
 
-      <div className="attendees">
-        <p><strong>Кількість гостей:</strong> {vote_count}</p>
-      </div>
+        <div className="attendees">
+          <p><strong>Кількість гостей:</strong> {vote_count}</p>
+        </div>
 
-      {response ? (
+        {response ? (
+          <div>
+            <p>Ваш вибір: {response}</p>
+          </div>
+        ) : (
+          <div>
+            <button className="green_button vote-button" onClick={handleOpenModal} value={true}>
+              Піду
+            </button>
+            <button className="green_button vote-button" onClick={handleOpenModal} value={false}>
+              Не піду
+            </button>
+          </div>
+        )}
         <div>
-          <p>Ваш вибір: {response}</p>
-        </div>
-      ) : (
-        <div>
-          <button className="green_button vote-button" onClick={handleOpenModal} value={true}>
-            Піду
-          </button>
-          <button className="green_button vote-button" onClick={handleOpenModal} value={false}>
-            Не піду
+          <button className="green_button" onClick={goBack}>
+            Повернутись на головну
           </button>
         </div>
-      )}
-      <div>
-        <button className="green_button" onClick={goBack}>
-          Повернутись на головну
-        </button>
       </div>
 
       {showModal && <Modal setShowModal={setShowModal} confirmResponse={confirmResponce} />}
-
       <Footer />
     </div>
   );
 };
+
+function EventDetailsPage() {
+  const [vote_count, setVoteCount] = useState(0);
+  const [response, setResponse] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [tempResponce, setTempResp] = useState(null);
+  const [attendees, setAttendees] = useState([])
+  const location = useLocation();
+  const event = location.state;
+  const eventDate = new Date(event.date);
+  const day = eventDate.getDate();
+  const monthNames = ['Січ', 'Лют', 'Бер', 'Кві', 'Тра', 'Чер', 'Лип', 'Сер', 'Вер', 'Жов', 'Лис', 'Гру'];
+  const month = monthNames[eventDate.getMonth()];
+
+
+
+  useEffect(() => {
+    if (event) {
+      axios.get("http://localhost:8080/api/votes/count", {
+        params:
+        {
+          id: event._id,
+          vote: true
+        }
+      })
+        .then((res) => {
+          setVoteCount(res.data);
+          console.log(res.data);
+        });
+      axios.get('http://localhost:8080/api/votes/guests',{
+        params:{
+          id:event._id,
+        }
+      }).then(res => {
+        console.log('guests:', res);
+        setAttendees(res.data);
+        setVoteCount(res.data.length);
+        console.log('attendees count:', res.data.length)
+      })
+    }
+  }, [event])
+
+  if (!event) {
+    return (
+      <div className="event-details-page">
+        <h1>Подію не знайдено</h1>
+        <Link to="/" className="back-link">
+          На головну
+        </Link>
+      </div>
+    );
+  }
+
+  const handleOpenModal = (v) => {
+    setShowModal(true);
+    setTempResp(v.target.value);
+  }
+  const confirmResponce = (userId) => {
+    setResponse(tempResponce);
+    axios.post("http://localhost:8080/api/votes", {
+      _vote: tempResponce,
+      _event: event._id,
+      _user: userId,
+    })
+  }
+
+  return (
+    <div className="event-details-page">
+      <Header />
+      <div className="event-details-container">
+        <div className="event-card">
+          {/* Event Header */}
+          <h1 className="event-title">{event.name+' '}
+            <div className="event-date-card">
+              <div>
+                <div className="date-day">{day}</div>
+                <div className="date-month">{month}</div>
+              </div>
+              <div className="date-vertical">{eventDate.getFullYear()}</div>
+            </div>
+          </h1>
+
+          {/* Event Category */}
+          <div className="event-category-container">
+            <span className="event-category">{event.category}</span>
+          </div>
+
+          {/* Event Description */}
+          <p className="event-description">{event.description}</p>
+
+          {/* Attendees Section */}
+          <div>
+            <h2 className="event-info-title">Записано {vote_count}</h2>
+            {attendees.length > 0 ? (<>
+              <ul className="event-attendees-list">
+                {attendees.map((attendee, index) => (
+                  <li key={index}>{attendee.name}: {attendee.email}</li>
+                ))}
+              </ul></>
+            ) : (
+              <p className="event-info-text">No attendees yet.</p>
+            )}
+          </div>
+
+          {/* Action Buttons */}
+          <div className="event-actions">
+            {response ? (
+          <div>
+            <p>Ваш вибір: {response}</p>
+          </div>
+        ) : (<>
+            <button type="button" className="green_button vote-button" onClick={handleOpenModal} value={true}>
+              Піду
+            </button>
+            <button type="button" className="green_button vote-button" onClick={handleOpenModal} value={false}>
+              Не піду
+            </button>
+        </>)}
+          </div>
+        </div>
+      </div>
+
+      {showModal && <Modal setShowModal={setShowModal} confirmResponse={confirmResponce} />}
+      <Footer />
+    </div>
+  );
+}
 
 export default EventDetailsPage;
--- server/routes/votes.js
+++ server/routes/votes.js
@@ -1,69 +1,89 @@
 const express = require('express');
 const router = express.Router();
 const Vote = require('../models/Vote')
-const {subscribe, unsubscribe} = require("../components/emailScheduler");
+const { subscribe, unsubscribe } = require("../components/emailScheduler");
 
 router.post('/', async (req, res) => {
-    if (!req.body) return res.sendStatus(400);
-    const { vote, event, user } = req.body;
-    console.log(req.body)
-    if (vote && event && user) {
-        const voteData = {
-            vote: vote,
-            event_id: event._id,
-            user_id: user._id
-        };
+  if (!req.body) return res.sendStatus(400);
+  const { vote, event, user } = req.body;
+  console.log(req.body)
+  if (vote && event && user) {
+    const voteData = {
+      vote: vote,
+      event_id: event._id,
+      user_id: user._id
+    };
 
-        const findedVote = await Vote.findOne({event_id: event._id, user_id: user._id});
-        if(findedVote){
-          if(findedVote.vote != vote){
-            updatedVote = await Vote.findOneAndUpdate(
-              {_id: findedVote._id},
-              {$set: {vote: vote}},
-              {upsert: true}
-            )
+    const findedVote = await Vote.findOne({ event_id: event._id, user_id: user._id });
+    if (findedVote) {
+      if (findedVote.vote != vote) {
+        updatedVote = await Vote.findOneAndUpdate(
+          { _id: findedVote._id },
+          { $set: { vote: vote } },
+          { upsert: true }
+        )
 
-            if(vote){
-              subscribe(user,event);
-            }
-            else unsubscribe(user,event);
-            res.status(200).json(updatedVote);
-          }
+        if (vote) {
+          subscribe(user, event);
         }
-        else{
-          await new Vote(voteData).save();
-          if(vote){
-            subscribe(user,event);
-          }
-          res.status(200).json(voteData);
-        }
+        else unsubscribe(user, event);
+        res.status(200).json(updatedVote);
+      }
     }
     else {
-        res.sendStatus(400);
+      await new Vote(voteData).save();
+      if (vote) {
+        subscribe(user, event);
+      }
+      res.status(200).json(voteData);
     }
+  }
+  else {
+    res.sendStatus(400);
+  }
 })
 
 router.get('/', async (req, res) => {
-    if (!req.query) return res.sendStatus(400);
-    const votes = await Vote.find();
-    res.status(200).json(votes);
+  if (!req.query) return res.sendStatus(400);
+  const votes = await Vote.find();
+  res.status(200).json(votes);
 })
 
 router.get('/count/', async (req, res) => {
-    const {id, vote} = req.query;
-    if (!id || !vote) {
-      return res.sendStatus(401);
+  const { id, vote } = req.query;
+  if (!id || !vote) {
+    return res.sendStatus(401);
+  }
+  else {
+    try {
+      const vote_count = await Vote.countDocuments({ event_id: id, vote: vote });
+      console.log(vote_count);
+      res.status(200).json(vote_count);
+    } catch (err) {
+      console.error('Помилка підрахунку кількості:', err);
+      res.sendStatus(500);
     }
-    else {
-      try {
-        const vote_count = await Vote.countDocuments({ event_id: id, vote: vote });
-        console.log(vote_count);
-        res.status(200).json(vote_count);
-      } catch (err) {
-        console.error('Помилка підрахунку кількості:', err);
-        res.sendStatus(500);
-      }
+  }
+})
+
+router.get('/guests/', async (req, res) => {
+  const { id } = req.query;
+  if (!id) {
+    return res.sendStatus(401);
+  }
+  else {
+    try {
+      const votes = await Vote.find({
+        event_id: id,
+        vote: true
+      }).populate('user_id');
+      const users = votes.map(vote => vote.user_id);
+      res.status(200).json(users);
+    } catch (err) {
+      console.error('Помилка підрахунку кількості:', err);
+      res.sendStatus(500);
     }
-  })
+  }
+})
 
 module.exports = router;
